afd71979bbce2bcf7fb3a57786b18d1f5933115c
Guy John <guy@rumblesan.com>
2015-04-19 14:38:17 +0100

    Adding a couple more tests

--- tests/app/interpreter_tests.js
+++ tests/app/interpreter_tests.js
@@ -2,6 +2,7 @@
 var ScopeHandler = require('../../src/app/scopeHandler').create();
 var Interpreter = require('../../src/app/interpreter').create(ScopeHandler);
 var Parser = require('../../src/app/parser').create();
+var StdLib = require('../../src/app/stdLib');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -25,13 +26,48 @@ var Parser = require('../../src/app/parser').create();
 
 module.exports = {
 
+    setUp: function (callback) {
+        // Make sure that stdlib functions are added to scope
+        var Core = {};
+        this.scope = ScopeHandler.createScope();
+        StdLib.addFunctions(Core, ScopeHandler, this.scope);
+        callback();
+    },
+
     'define adds value to scope': function (test) {
         var program = '(define a 3)';
         var ast = Parser.parse(program);
-        var scope = ScopeHandler.createScope();
-        Interpreter.evaluate(scope, ast);
-        test.equal(ScopeHandler.get(scope, 'a'), 3);
+        Interpreter.evaluate(this.scope, ast);
+        test.equal(ScopeHandler.get(this.scope, 'a'), 3);
+        test.done();
+    },
+
+    'define function adds function to scope': function (test) {
+        var program = '(define (sum a b) (+ a b))';
+        var ast = Parser.parse(program);
+        Interpreter.evaluate(this.scope, ast);
+        test.ok(ScopeHandler.get(this.scope, 'sum'));
+        test.done();
+    },
+
+    'defined functions can be run': function (test) {
+        var program = "(define (sum a b) (+ a b))\n(define c (sum 1 2))";
+        var ast = Parser.parse(program);
+        Interpreter.evaluate(this.scope, ast);
+        test.ok(ScopeHandler.get(this.scope, 'sum'));
+        test.equal(ScopeHandler.get(this.scope, 'c'), 3);
+        test.done();
+    },
+
+    'lambdas can be run': function (test) {
+        var program = "(define l (lambda (a b) (+ a b)))\n(define c (l 1 2))";
+        var ast = Parser.parse(program);
+        Interpreter.evaluate(this.scope, ast);
+        test.ok(ScopeHandler.get(this.scope, 'l'));
+        test.equal(ScopeHandler.get(this.scope, 'c'), 3);
         test.done();
     }
+
+
 };
 
