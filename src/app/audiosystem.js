8f6dd479b572e3b36b631da080037df98bd22a08
Guy John <guy@rumblesan.com>
2015-04-25 16:41:53 +0100

    Actually have a sensible api for defining synths!

--- src/app/audiosystem.js
+++ src/app/audiosystem.js
@@ -1,6 +1,8 @@
 /*jslint browser: true */
 /*global AudioContext */
 
+var AudioHelpers = require('./audio/helpers');
+var AudioGlobals = require('./audio/globals');
 var Error = require('./error');
 
 var createContext = function (w) {
@@ -20,54 +22,165 @@ var createSystem = function (context) {
 
     var internal = {};
 
-    internal.createOsc = function (oscRep) {
-        var oscillator = context.createOscillator();
-        oscillator.type = oscRep.wave;
-        oscillator.start();
-        var play = function (freq) {
-            oscillator.frequency.value = freq;
-        };
-        var stop = function () {
-        };
+    internal.createConstant = function (audioTargetNode, graphRep) {
+        audioTargetNode.set(graphRep.value);
         return {
-            node: oscillator,
-            play: play,
-            stop: stop
-        };
-    }
-
-    internal.createEnv = function (envRep) {
-        var amp = context.createGain();
-        amp.gain.value = 0;
-
-        var source = AudioSystem.createDSPGraph(envRep.source);
-        source.node.connect(amp);
-
-        var play = function (freq) {
-            source.play(freq);
-            amp.gain.linearRampToValueAtTime(1, context.currentTime + envRep.attack);
-        };
-        var stop = function () {
-            amp.gain.linearRampToValueAtTime(0, context.currentTime + envRep.decay);
-            setTimeout(function () {
-                source.stop();
-            }, envRep.decay * 1000);
-        };
-        return {
-            node: amp,
-            play: play,
-            stop: stop
+            params: []
         };
     };
 
-    AudioSystem.createDSPGraph = function(graphRep) {
+    internal.createParam = function (audioTargetNode, graphRep) {
+        var paramName = graphRep.name;
+        var defaultValue = graphRep.defaultValue;
+        audioTargetNode.set(defaultValue);
+        var paramParams = {};
+        paramParams.params = [paramName];
+        paramParams[paramName] = [
+            function (newValue) {
+                audioTargetNode.set(newValue);
+            }
+        ];
+        return paramParams;
+    };
+
+    internal.createOscillator = function (audioTargetNode, graphRep) {
+        var oscillator = context.createOscillator();
+        oscillator.start();
+
+        var waveParam = AudioSystem.evaluateDSPGraph(
+            oscillator.getWaveParam(),
+            graphRep.wave
+        );
+
+        var freqParam = AudioSystem.evaluateDSPGraph(
+            oscillator.frequency,
+            graphRep.frequency
+        );
+
+        oscillator.connect(audioTargetNode);
+
+        return AudioHelpers.mergeNodeParams([waveParam, freqParam]);
+    };
+
+    internal.createEnvelope = function (audioTargetNode, graphRep) {
+
+        var envParams = {};
+        envParams.attack = {
+            value: 0,
+            set: function (newValue) {
+                envParams.attack.value = newValue;
+            },
+            get: function () {
+                return envParams.attack.value;
+            }
+        };
+        envParams.decay = {
+            value: 0,
+            set: function (newValue) {
+                envParams.decay.value = newValue;
+            },
+            get: function () {
+                return envParams.decay.value;
+            }
+        };
+
+        var attackParams = AudioSystem.evaluateDSPGraph(
+            envParams.attack,
+            graphRep.attack
+        );
+
+        var decayParams = AudioSystem.evaluateDSPGraph(
+            envParams.decay,
+            graphRep.decay
+        );
+
+        var trigger = function () {
+            var t = context.currentTime + envParams.attack.value;
+            audioTargetNode.linearRampToValueAtTime(
+                1.0, t
+            );
+        };
+        var stop = function () {
+            var t = context.currentTime + envParams.decay.value;
+            audioTargetNode.linearRampToValueAtTime(
+                0.0, t
+            );
+        };
+        var triggerParams = {
+            params: ['trigger', 'stop'],
+            trigger: [trigger],
+            stop: [stop]
+        };
+
+        return AudioHelpers.mergeNodeParams([attackParams, decayParams, triggerParams]);
+    };
+
+    internal.createFilter = function (audioTargetNode, graphRep) {
+        var filter = context.createBiquadFilter();
+
+        var sourceParams = AudioSystem.evaluateDSPGraph(
+            filter,
+            graphRep.source
+        );
+
+        var filterTypeParam = AudioSystem.evaluateDSPGraph(
+            filter.getFilterTypeParam(),
+            graphRep.filterType
+        );
+
+        var freqParam = AudioSystem.evaluateDSPGraph(
+            filter.frequency,
+            graphRep.frequency
+        );
+
+        var resonanceParam = AudioSystem.evaluateDSPGraph(
+            filter.Q,
+            graphRep.resonance
+        );
+
+        filter.connect(audioTargetNode);
+
+        return AudioHelpers.mergeNodeParams([sourceParams, filterTypeParam, freqParam, resonanceParam]);
+    };
+
+    internal.createAmp = function (audioTargetNode, graphRep) {
+        var amp = context.createGain();
+        amp.gain.value = 0;
+
+        var sourceParams = AudioSystem.evaluateDSPGraph(
+            amp,
+            graphRep.source
+        );
+
+        var volumeParams = AudioSystem.evaluateDSPGraph(
+            amp.gain,
+            graphRep.volume
+        );
+        amp.connect(audioTargetNode);
+
+        return AudioHelpers.mergeNodeParams([sourceParams, volumeParams]);
+    };
+
+    AudioSystem.evaluateDSPGraph = function(audioTargetNode, graphRep) {
         var result;
         switch (graphRep.type) {
-            case 'OSCILLATOR':
-                result = internal.createOsc(graphRep);
+            case 'CONSTANT':
+                result = internal.createConstant(audioTargetNode, graphRep);
                 break;
-            case 'ENVELOPE':
-                result = internal.createEnv(graphRep);
+            case 'PARAM':
+                result = internal.createParam(audioTargetNode, graphRep);
+                break;
+            case 'ARENVELOPE':
+                result = internal.createEnvelope(audioTargetNode, graphRep);
+                break;
+            case 'OSCILLATOR':
+                result = internal.createOscillator(audioTargetNode, graphRep);
+                break;
+            case 'FILTER':
+                result = internal.createFilter(audioTargetNode, graphRep);
+                break;
+            case 'AMP':
+                result = internal.createAmp(audioTargetNode, graphRep);
                 break;
             default:
                 throw Error.create("Unknown DSP graph type: " + graphRep.type);
@@ -75,15 +188,78 @@ var createSystem = function (context) {
         return result;
     };
 
-    AudioSystem.connectDSPGraph = function(dspGraph) {
-        dspGraph.node.connect(context.destination);
+    /**
+     * returns
+     *     Synth: {
+     *       params: [paramNames, ...],
+     *       paramName1: [setFunctions, ...]
+     *     }
+     **/
+    AudioSystem.createSynth = function (dspGraph) {
+        return AudioSystem.evaluateDSPGraph(
+            context.destination,
+            dspGraph
+        );
     };
 
-    AudioSystem.triggerGraph = function(dspGraph, playLength, freq) {
-        dspGraph.play(freq);
-        setTimeout(function () {
-            dspGraph.stop();
-        }, playLength * 1000);
+    AudioSystem.setSynthParam = function (synth, paramName, value) {
+        var i;
+        if (synth[paramName] === undefined) {
+            throw Error.create('Synth does not have ' + paramName + ' parameter');
+        } else {
+            for (i = 0; i < synth[paramName].length; i += 1) {
+                synth[paramName][i](value);
+            }
+        }
+    };
+
+    AudioSystem.startSynth = function (synth, parameterList) {
+        var i, t;
+        var paramName, paramValue;
+        if (synth.trigger === undefined) {
+            throw Error.create('Synth does not have trigger parameter');
+        } else {
+            for (i = 0; i < parameterList.length; i += 2) {
+                paramName  = parameterList[i];
+                paramValue = parameterList[i+1];
+                AudioSystem.setSynthParam(synth, paramName, paramValue);
+            }
+            for (t = 0; t < synth.trigger.length; t += 1) {
+                synth.trigger[t]();
+            }
+        }
+    };
+
+    AudioSystem.stopSynth = function (synth) {
+        var i, t;
+        var paramName, paramValue;
+        if (synth.stop === undefined) {
+            throw Error.create('Synth does not have stop parameter');
+        } else {
+            for (t = 0; t < synth.stop.length; t += 1) {
+                synth.stop[t]();
+            }
+        }
+    };
+
+    AudioSystem.playSynth = function (synth, length, parameterList) {
+        var i, t;
+        var paramName, paramValue;
+        if (synth.trigger === undefined) {
+            throw Error.create('Synth does not have trigger parameter');
+        } else {
+            for (i = 0; i < parameterList.length; i += 2) {
+                paramName  = parameterList[i];
+                paramValue = parameterList[i+1];
+                AudioSystem.setSynthParam(synth, paramName, paramValue);
+            }
+            for (t = 0; t < synth.trigger.length; t += 1) {
+                synth.trigger[t]();
+            }
+            setTimeout(function () {
+                AudioSystem.stopSynth(synth);
+            }, length * 1000);
+        }
     };
 
     return AudioSystem;
--- src/app/stdlib/audio.js
+++ src/app/stdlib/audio.js
@@ -1,35 +1,81 @@
 
+var DspGraph = require('../audio/dspGraph');
+
 var addFunctions = function (Core, ScopeHandler, scope) {
 
-    var arEnv = function(source, attack, decay) {
-        return {
-            type: 'ENVELOPE',
-            source: source,
-            attack: attack,
-            decay: decay
-        };
-    };
-    ScopeHandler.addFF(scope, 'arEnv', arEnv);
+    ScopeHandler.addFF(scope, 'param',
+        function(name, defaultValue) {
+            return DspGraph.Param(name, defaultValue);
+        }
+    );
 
-    var osc = function(wave) {
-        return {
-            type: 'OSCILLATOR',
-            wave: wave
-        };
-    };
-    ScopeHandler.addFF(scope, 'osc', osc);
+    ScopeHandler.addFF(scope, 'arEnv',
+        function(attack, decay) {
+            return DspGraph.AREnvelope(attack, decay);
+        }
+    );
 
-    var createDSPGraph = function(graphRep) {
-        var graph = Core.Audio.createDSPGraph(graphRep);
-        Core.Audio.connectDSPGraph(graph);
-        return graph;
-    };
-    ScopeHandler.addFF(scope, 'createDSPGraph', createDSPGraph);
+    ScopeHandler.addFF(scope, 'osc',
+        function(frequency, wave) {
+            return DspGraph.Oscillator(frequency, wave);
+        }
+    );
 
-    var play = function(dspGraph, playLength, freq) {
-        Core.Audio.triggerGraph(dspGraph, playLength, freq);
-    };
-    ScopeHandler.addFF(scope, 'play', play);
+    ScopeHandler.addFF(scope, 'filter',
+        function(source, filterType, frequency, resonance) {
+            return DspGraph.Filter(source, filterType, frequency, resonance);
+        }
+    );
+
+    ScopeHandler.addFF(scope, 'amp',
+        function(source, volume) {
+            return DspGraph.Amp(source, volume);
+        }
+    );
+
+
+    ScopeHandler.addFF(scope, 'createSynth',
+        function(dspGraph) {
+            return Core.Audio.createSynth(dspGraph);
+        }
+    );
+
+    ScopeHandler.addFF(scope, 'play',
+        function(synth, playLength) {
+            Core.Audio.playSynth(synth, playLength, []);
+        }
+    );
+
+    ScopeHandler.addFF(scope, 'start',
+        function(synth, parameterList) {
+            Core.Audio.startSynth(synth, parameterList);
+        }
+    );
+
+    ScopeHandler.addFF(scope, 'stop',
+        function(synth) {
+            Core.Audio.stopSynth(synth);
+        }
+    );
+
+    ScopeHandler.addFF(scope, 'set',
+        function(synth, paramName, paramValue) {
+            Core.Audio.setSynthParam(synth, paramName, paramValue);
+        }
+    );
+
+    ScopeHandler.addFF(scope, 'setMultiple',
+        function(synth, parameterList) {
+            var i;
+            for (i = 0; i < parameterList.length; i += 2) {
+                Core.Audio.setSynthParam(
+                    synth,
+                    parameterList[i],
+                    parameterList[i+1]
+                );
+            }
+        }
+    );
 
 };
 
