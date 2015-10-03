7051c2a444df0cb1c6117926c22743f349ce7521
Guy John <guy@rumblesan.com>
2015-10-03 11:52:09 +0100

    Cleaning up dispatcher

--- src/app/util/dispatcher.js
+++ src/app/util/dispatcher.js
@@ -1,33 +1,31 @@
 
-var createDispatcher = function () {
-
-    var DispatcherObj = {};
-    var callbacks = {};
-
-    DispatcherObj.register = function (eventName, callback) {
-        callbacks[eventName] = callbacks[eventName] || [];
-        callbacks[eventName].push(callback);
-    };
-
-    DispatcherObj.dispatch = function (eventName /* , args... */) {
-        var i;
-        var cbList = callbacks[eventName] || [];
-        for (i = 0; i < cbList.length; i += 1) {
-            cbList[i].apply(this, Array.prototype.slice.call(arguments, 1));
-        }
-    };
-    DispatcherObj.unregister = function (eventName, callback) {
-        var cbList = callbacks[eventName];
-        var fIds = cbList.indexOf(callback);
-        if (fIds > -1) {
-            cbList.splice(fIds, 1);
-        }
-    };
-
-    return DispatcherObj;
-
-};
-
 module.exports = {
-    create: createDispatcher
+
+    create: function () {
+
+        let DispatcherObj = {};
+        let callbacks = {};
+
+        DispatcherObj.register = function (eventName, callback) {
+            callbacks[eventName] = callbacks[eventName] || [];
+            callbacks[eventName].push(callback);
+        };
+
+        DispatcherObj.dispatch = function (eventName , ...args) {
+            const cbList = callbacks[eventName] || [];
+            cbList.map((cb) => cb.apply({}, args));
+        };
+
+        DispatcherObj.unregister = function (eventName, callback) {
+            const cbList = callbacks[eventName] || [];
+            const cbId = cbList.indexOf(callback);
+            if (cbId > -1) {
+                cbList.splice(cbId, 1);
+            }
+        };
+
+        return DispatcherObj;
+    }
+
 };
+
