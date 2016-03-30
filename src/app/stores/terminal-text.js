d2c3cca964da79e8910290ad87340373f41cdcc9
Guy John <guy@rumblesan.com>
2016-03-30 16:38:59 +0100

    Change terminal text internal name

--- src/app/stores/terminal-text.js
+++ src/app/stores/terminal-text.js
@@ -6,7 +6,7 @@ export const create = (dispatcher) => {
     const state = {
         lines: []
     };
-    const AppState = Object.assign({}, EventEmitter.prototype, {
+    const TerminalText = Object.assign({}, EventEmitter.prototype, {
 
         emitChange: function(payload) {
             this.emit(CHANGE_EVENT, payload);
@@ -47,18 +47,18 @@ export const create = (dispatcher) => {
     dispatcher.register((action) => {
         switch (action.type) {
         case 'LOAD-DEMO':
-            AppState.addInfo(`Loading Demo: ${action.demoName}`);
+            TerminalText.addInfo(`Loading Demo: ${action.demoName}`);
             break;
         case 'LOAD-TUTORIAL':
-            AppState.addInfo(`Loading Tutorial: ${action.tutorialName}`);
+            TerminalText.addInfo(`Loading Tutorial: ${action.tutorialName}`);
             break;
         case 'SET-KEY-BINDINGS':
-            AppState.addInfo(`Setting key bindings: ${action.keyBindingName}`);
+            TerminalText.addInfo(`Setting key bindings: ${action.keyBindingName}`);
             break;
         default:
             // do nothing
         }
     });
 
-    return AppState;
+    return TerminalText;
 }
