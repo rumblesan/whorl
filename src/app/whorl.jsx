cd973a77a84e271d534c4c902e7b707004abe2e6
Guy John <guy@rumblesan.com>
2016-03-30 13:48:55 +0100

    Move creation of app parts into Whorl controller

--- src/app/whorl.jsx
+++ src/app/whorl.jsx
@@ -1,33 +1,71 @@
 
+// Libs
+import React from 'react';
+import {Dispatcher} from 'flux';
+
+// UI Elements
 import NavBar from './ui/navbar';
 import Terminal from './ui/terminal';
 import Codemirror from 'react-codemirror';
 
-import * as Core        from './core';
+// Stores
+import * as TerminalTextStore from './stores/terminal-text';
 
-import React from 'react';
+import * as Core        from './core';
+import * as AudioSystem from './audio';
+
+import * as Demos     from '../generated/demos';
+import * as Tutorials from '../generated/tutorials';
+
+import * as Actions from './ui/actions';
+
+
+const dispatcher = new Dispatcher();
+
+const actions = Actions.create(dispatcher);
+
+const terminalText = TerminalTextStore.create(dispatcher);
 
 export default React.createClass({
     getInitialState: function () {
-        return {};
+        return {
+            systems: {
+                audio: 'STOPPED'
+            },
+            terminal: terminalText.getState()
+        };
     },
     componentWillMount: function () {
-        console.log('mounting');
-        this.core = Core.create(this.props.audioContext, this.props.dispatcher);
+        try {
+            this.audioContext = AudioSystem.createContext(window);
+            this.setState({systems: {audio: 'STARTED'}});
+        } catch (e) {
+            console.log(e);
+            this.setState({systems: {audio: 'ERROR'}});
+        }
+        this.core = Core.create(this.audioContext, dispatcher);
+    },
+    componentDidMount: function () {
+        terminalText.addChangeListener(this._onChange);
     },
     render: function () {
-        console.log(this.props);
         return (
             <div>
                 <NavBar
-                    demos={this.props.demos}
-                    tutorials={this.props.tutorials}
-                    actions={this.props.actions}
+                    demos={Demos}
+                    tutorials={Tutorials}
+                    actions={actions}
                 />
                 <Codemirror value={'Hello, World!'} />
-                <Terminal />
+                <Terminal terminal={this.state.terminal}/>
             </div>
         );
+    },
+    _onChange: function () {
+        this.setState({
+            systems: this.state.systems,
+            terminal: terminalText.getState()
+        });
     }
 });
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,41 +2,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Dispatcher} from 'flux';
-
 
 import Whorl from './app/whorl';
 
-import * as AudioSystem from './app/audio';
+ReactDOM.render(
+    <Whorl />,
+    document.getElementById('app')
+);
 
-import * as Demos     from './generated/demos';
-import * as Tutorials from './generated/tutorials';
-
-import * as Actions from './app/ui/actions';
-
-const dispatcher = new Dispatcher();
-
-const actions = Actions.create(dispatcher);
-
-var audioContext = null;
-
-try {
-    audioContext = AudioSystem.createContext(window);
-    console.log(audioContext);
-} catch (e) {
-    console.log(e);
-}
-
-if (audioContext !== null) {
-    console.log('Rendering');
-    ReactDOM.render(
-        <Whorl
-            demos={Demos}
-            tutorials={Tutorials}
-            actions={actions}
-            audioContext={audioContext}
-            dispatcher={dispatcher}
-        />,
-        document.getElementById('app')
-    );
-}
